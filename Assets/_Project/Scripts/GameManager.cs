7b805118393e4f8b0b6fb52cadc0ac1924d2fe30
TaranisElsu <taraniselsu@gmail.com>
2022-02-09 22:50:23 -0500

    Part 4 complete: load a level based on how many players are connected

--- Assets/_Project/Scripts/GameManager.cs
+++ Assets/_Project/Scripts/GameManager.cs
@@ -1,8 +1,32 @@
 using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.LogFormat("OnPlayerEnteredRoom: {0}", newPlayer.NickName);
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("OnPlayerEnteredRoom: IsMasterClient");
+            LoadArena();
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.LogFormat("OnPlayerLeftRoom: {0}", otherPlayer.NickName);
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("OnPlayerLeftRoom: IsMasterClient");
+            LoadArena();
+        }
+    }
+
     public override void OnLeftRoom()
     {
         SceneManager.LoadScene(0);
@@ -11,5 +35,17 @@ public class GameManager : MonoBehaviourPunCallbacks
     public void LeaveRoom()
     {
         PhotonNetwork.LeaveRoom();
+    }
+
+    private void LoadArena()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogError("Trying to load a level but we are not the master client");
+        }
+
+        string levelName = "Room for " + PhotonNetwork.CurrentRoom.PlayerCount;
+        Debug.LogFormat("Loading level: {0}", levelName);
+        PhotonNetwork.LoadLevel(levelName);
     }
 }
--- Assets/_Project/Scripts/Launcher.cs
+++ Assets/_Project/Scripts/Launcher.cs
@@ -8,6 +8,8 @@ public class Launcher : MonoBehaviourPunCallbacks
     [SerializeField] private GameObject progressLabel;
     [SerializeField] private byte maxPlayersPerRoom = 4;
 
+    private bool isConnecting = false;
+
     private void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
@@ -30,14 +32,18 @@ public class Launcher : MonoBehaviourPunCallbacks
         }
         else
         {
-            PhotonNetwork.ConnectUsingSettings();
+            isConnecting = PhotonNetwork.ConnectUsingSettings();
         }
     }
 
     public override void OnConnectedToMaster()
     {
         Debug.Log("OnConnectedToMaster");
-        PhotonNetwork.JoinRandomRoom();
+        if (isConnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+            isConnecting = false;
+        }
     }
 
     public override void OnDisconnected(DisconnectCause cause)
@@ -45,16 +51,23 @@ public class Launcher : MonoBehaviourPunCallbacks
         Debug.LogWarningFormat("OnDisconnected: {0}", cause);
         controlPanel.SetActive(true);
         progressLabel.SetActive(false);
+        isConnecting = false;
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        Debug.LogFormat("OnJoinRandomFailed: {0} {1}", returnCode, message);
+        Debug.LogFormat("OnJoinRandomFailed: {0} [{1}]. Creating a new room.", message, returnCode);
         PhotonNetwork.CreateRoom("room", new RoomOptions() { MaxPlayers = maxPlayersPerRoom });
     }
 
     public override void OnJoinedRoom()
     {
         Debug.LogFormat("OnJoinedRoom: {0}", PhotonNetwork.CurrentRoom.Name);
+
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            Debug.Log("OnJoinedRoom: loading level 'Room for 1'");
+            PhotonNetwork.LoadLevel("Room for 1");
+        }
     }
 }
